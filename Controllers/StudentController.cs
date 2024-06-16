292e6351a8c0cb6632125356adc6e89641ee26e3
nuthapon.s <nuthapon.s@softdebut.com>
2024-06-16 16:08:40 +0700

    feat: first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using BasicASPTutorial.Data;
+using BasicASPTutorial.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicASPTutorial.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private readonly ApplicationDBContext _db;
+
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]   
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Student> students = await _db.Students.ToListAsync();
+            return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Students.Add(student);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _db.Students.FindAsync(id);
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+using BasicASPTutorial.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicASPTutorial.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set;}
+    }
+}
